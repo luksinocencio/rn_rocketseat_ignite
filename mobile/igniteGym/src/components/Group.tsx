e1e9f8ce9a0e1a9f171277f4b603c0955dd2b4c4
luksinocencio <luksinocencio@gmail.com>
2023-10-26 13:33:01 -0300

    feat: cria component Group na horizontal

--- mobile/igniteGym/src/components/Group.tsx
+++ mobile/igniteGym/src/components/Group.tsx
@@ -1,17 +1,34 @@
-import { Text } from 'native-base'
+import { Text, Pressable, IPressableProps, Hidden } from 'native-base'
 
-type Props = {
+type Props = IPressableProps & {
   name: string
+  isActive: boolean
 }
 
-export function Group({ name }: Props) {
+export function Group({ name, isActive, ...rest }: Props) {
   return (
-    <Text
-      color="gray.200"
-      textTransform={'uppercase'}
-      fontSize="xs"
-      fontWeight="bold">
-      {name}
-    </Text>
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent={'center'}
+      alignItems={'center'}
+      overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: 'green.500',
+        borderWidth: 1,
+      }}
+      {...rest}>
+      <Text
+        color={isActive ? 'green.500' : 'gray.200'}
+        textTransform={'uppercase'}
+        fontSize="xs"
+        fontWeight="bold">
+        {name}
+      </Text>
+    </Pressable>
   )
 }
