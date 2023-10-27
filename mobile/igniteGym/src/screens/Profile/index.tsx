336b22cc94ef6b759a3782bc4e40c00c7cd9a997
luksinocencio <luksinocencio@gmail.com>
2023-10-27 01:05:14 -0300

    feat: profile screen

--- mobile/igniteGym/src/screens/Profile/index.tsx
+++ mobile/igniteGym/src/screens/Profile/index.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { VStack, Text, ScrollView, Center } from 'native-base'
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="gray.100">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{
+              uri: 'https://gravatar.com/avatar/c8b50b95553604091c31454162752a35?s=400&d=robohash&r=x',
+            }}
+            alt="Foto do usuário"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
