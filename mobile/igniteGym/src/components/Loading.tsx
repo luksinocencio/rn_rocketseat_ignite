57bbc49410d86743479ddd5b816520e996b08967
luksinocencio <luksinocencio@gmail.com>
2023-10-28 11:09:51 -0300

    delete: remove react native cli project

--- mobile/igniteGym/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Spinner, useTheme } from "native-base";
-
-export function Loading() {
-  return (
-    <Center flex={1} bg={"gray.700"}>
-      <Spinner size={"lg"} color='green.500' />
-    </Center>
-  );
-}
